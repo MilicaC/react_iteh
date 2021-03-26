12c2db1ce640eb6dabc0a26e921c8154029815e9
Milica Cakarevic <mc20170147@student.fon.bg.ac.rs>
2021-03-26 23:24:06 +0100

    commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">O nama</h1>
+      <p>
+        
+
+      </p>
+    </section>
+  );
+}
